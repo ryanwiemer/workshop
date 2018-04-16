cb0e2014b39810e840203b2c1c596e86cccbe33c
Ryan Wiemer <ryan@ryanwiemer.com>
2018-04-16 00:19:19 -0700

    remove scrollmagic for now

--- src/components/Biography.js
+++ src/components/Biography.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
 import Img from 'gatsby-image'
-import ScrollMagic from 'ScrollMagic';
 
 const Wrapper = styled.div`
   position: relative;
