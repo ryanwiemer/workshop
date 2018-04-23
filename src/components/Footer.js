c44f8ae823f3f65250f2f9f0c43adc69aa6c1096
Ryan Wiemer <ryan@ryanwiemer.com>
2018-04-23 12:09:41 -0700

    Fix logo attribution

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -35,7 +35,7 @@ const Footer = (props) => (
   <Wrapper>
     <List>
       <Item>
-        <a href="https://www.contentful.com/" rel="nofollow" target="_blank"><img src="https://images.contentful.com/fo9twyrwpveg/44baP9Gtm8qE2Umm8CQwQk/c43325463d1cb5db2ef97fca0788ea55/PoweredByContentful_LightBackground.svg" style={{maxWidth:"100px",width:"100%"}} alt="Powered by Contentful" /></a>
+        <a href="https://www.contentful.com/" rel="nofollow" target="_blank"><img src="https://images.ctfassets.net/fo9twyrwpveg/44baP9Gtm8qE2Umm8CQwQk/c43325463d1cb5db2ef97fca0788ea55/PoweredByContentful_LightBackground.svg" style={{width:"100px"}} alt="Powered by Contentful" /></a>
       </Item>
       <Item><Up/></Item>
     </List>
