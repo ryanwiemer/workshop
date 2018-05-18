996bfec79b420c4467f75ac3d9d50d226a148913
Ryan Wiemer <ryan@ryanwiemer.com>
2018-05-18 13:53:22 -0700

    Move icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
         background_color: '#f7f0eb',
         theme_color: '#fffff',
         display: 'minimal-ui',
-        icon: 'src/images/icon.png', // This path is relative to the root of the site.
+        icon: 'icons/icon.png', // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-offline`,
